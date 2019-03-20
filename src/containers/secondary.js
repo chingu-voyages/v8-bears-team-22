9f4d6c6b51a464ed7009dbd189c991fe9a2f3b43
Michael <nt.prying.eyes@gmail.com>
2019-03-20 19:16:48 +0800

    Added Instructions and Minor Formatting

--- src/containers/secondary.js
+++ src/containers/secondary.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
-const Secondary=()=>{
-    return (
-        <div>
-            <Link to="/">Secondary</Link>
-        </div>
-    )
-}
+const Secondary = () => {
+  return (
+    <div>
+      <Link to="/">Secondary</Link>
+    </div>
+  );
+};
 
-export default Secondary
+export default Secondary;

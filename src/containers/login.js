e637a64b3316434f406b2875c72a7005f16832aa
maki <maki.tolentino.03@gmail.com>
2019-03-27 06:20:05 +0800

    Login - Added component logic

--- src/containers/login.js
+++ src/containers/login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {FormControl, Input, InputLabel, Paper, Button} from "@material-ui/core";
+import '../css/login.css';
 
 export default class Login extends Component {
 
@@ -11,20 +12,50 @@ export default class Login extends Component {
     };
   }
 
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0
+  }
+
+  onFormSubmit = event => {
+    event.preventDefault();
+  };
+
+  onInputChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  onLoginClick = () => {
+    // TODO add submit to server endpoint
+    console.log(this.state);
+  };
+
   render() {
     return (
-      <div>
-        <Paper>
-          <form>
+      <div className="login">
+        <Paper className="paper">
+          <form onSubmit={this.onFormSubmit}
+                className="login-form">
             <FormControl fullWidth>
               <InputLabel>Email</InputLabel>
-              <Input id="email" name="email"/>
+              <Input id="email"
+                     name="email"
+                     type="email"
+                     onChange={this.onInputChange}/>
             </FormControl>
             <FormControl fullWidth>
               <InputLabel>Password</InputLabel>
-              <Input id="password" name="password" type="password"/>
+              <Input id="password"
+                     name="password"
+                     type="password"
+                     onChange={this.onInputChange}/>
             </FormControl>
-            <Button type="submit" fullWidth color="primary">Login</Button>
+            <Button type="submit"
+                    fullWidth
+                    disabled={!this.validateForm()}
+                    onClick={this.onLoginClick}
+                    color="primary">Login</Button>
           </form>
         </Paper>
       </div>

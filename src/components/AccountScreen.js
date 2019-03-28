2db399b32a333484b069c7b19c299aab7d5fa84b
denibulkashvili <denibulkashvili@gmail.com>
2019-03-28 21:42:05 +0700

    account forms WIP

--- src/components/AccountScreen.js
+++ src/components/AccountScreen.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react'
-// import {Button, FormControl, Input, InputLabel, Paper} from "@material-ui/core"
+import {FormLabel, Paper, TextField} from '@material-ui/core';
+import '../css/account.css';
 
 
 class AccountScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: "",
-      password: "",
+      email: "johndoe@gmail.com",
+      password: "password",
+      name: "John Doe",
       isLoggedIn: false
     }
   }
 
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
 
   render() {
     return (
       <div className="account">
-        <form className="account-personal">
-          Account Screen
-        </form>
+        <FormLabel className="account-form-label">PERSONAL</FormLabel>
+        <Paper className="account-paper">
+          <form className="account-form">
+            <TextField
+              id="standard-uncontrolled"
+              label="Name"
+              className="text-field"
+              value={this.state.name}
+              onChange={this.handleChange('name')}
+              margin="normal"
+            />
+            <TextField
+              id="standard-uncontrolled"
+              label="Email"
+              className="text-field"
+              value={this.state.email}
+              onChange={this.handleChange('email')}
+              margin="normal"
+            />
+          </form>
+        </Paper>
       </div>
     )
   }
